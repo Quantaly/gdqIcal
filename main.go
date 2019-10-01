172d06c3cfa3363902dc0c8d4cd9a49f7e620acc
Kai Page <kaibug@gmail.com>
2019-10-01 18:58:49 -0500

    Schedule URL is based on environment variable

--- main.go
+++ main.go
@@ -11,12 +11,17 @@ import (
 func main() {
 	log.Println("Preparing to serve")
 
+	scheduleURL, present := os.LookupEnv("SCHEDULE_URL")
+	if !present {
+		scheduleURL = "https://gamesdonequick.com/schedule"
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./web/static")))
 
 	http.HandleFunc("/out.ics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/calendar")
 		w.WriteHeader(200)
-		lib.GenerateCalendar(w, "https://gamesdonequick.com/schedule/27") // GDQx 2019
+		lib.GenerateCalendar(w, scheduleURL)
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
