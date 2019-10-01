2254f4b16e30d4b2f1d4f7fecab168fd569c11d9
Kai Page <kaibug@gmail.com>
2019-10-01 18:42:10 -0500

    Static serving of proper index.html

--- main.go
+++ main.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"github.com/Quantaly/gdqIcal/lib"
 )
 
-var templates = template.Must(template.ParseFiles("templates/index.html"))
-
 func main() {
 	log.Println("Preparing to serve")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := templates.ExecuteTemplate(w, "index.html", struct{}{})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	http.Handle("/", http.FileServer(http.Dir("./web/static")))
 
 	http.HandleFunc("/out.ics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/calendar")
